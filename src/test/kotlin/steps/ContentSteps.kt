f7f2f58849079718ed25e773958d254005170186
A.J. Sabako <a.j.sabako@gmail.com>
2019-06-21 22:16:29 -0500

    add circleci yml for hello world

--- src/test/kotlin/steps/ContentSteps.kt
+++ src/test/kotlin/steps/ContentSteps.kt
@@ -1,15 +1,13 @@
 package steps
 
-import support.World
 import cucumber.api.java.en.Then
-import io.cucumber.datatable.DataTable
 import org.junit.Assert.assertTrue
 import org.openqa.selenium.By
+import support.World
 
 class ContentSteps(var world: World) {
     @Then("^the following text should display:$")
-    fun `the following text should display`(table: DataTable) {
-        val tableText = table.asList()
+    fun `the following text should display`(tableText: List<String>) {
         for (text in tableText) {
             val locator = "//*[contains(text(), '$text')]"
             assertTrue(world.driver.findElement(By.xpath(locator)).isDisplayed)
@@ -17,8 +15,7 @@ class ContentSteps(var world: World) {
     }
 
     @Then("^the text for element \"h3\" should be one of the following:$")
-    fun `the text for element {element} should be one of the following`(table: DataTable) {
-        val tableText = table.asList()
+    fun `the text for element {element} should be one of the following`(tableText: List<String>) {
         val elementText = world.driver.findElement(By.xpath("//h3")).text
         assertTrue(tableText.contains(elementText))
     }
