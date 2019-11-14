debaed7db41bd2f67502bb705e84a30c068a0742
A.J. Sabako <a.j.sabako@gmail.com>
2019-11-14 12:24:01 -0600

    add negative login test

--- src/test/kotlin/steps/LoginSteps.kt
+++ src/test/kotlin/steps/LoginSteps.kt
@@ -1,6 +1,7 @@
 package steps
 
 import cucumber.api.java.en.When
+import io.cucumber.datatable.DataTable
 import pages.LoginPage
 import support.World
 
@@ -10,6 +11,17 @@ class LoginSteps(var world: World) {
         val loginPage = LoginPage(world)
         loginPage.populateUsername("tomsmith")
         loginPage.populatePassword("SuperSecretPassword!")
+        loginPage.clickLoginButton()
+    }
+
+    @When("I log in:")
+    fun `I log in with credentials`(table: DataTable) {
+        val loginPage = LoginPage(world)
+        val tableMap =  table.asMaps().first()
+        val username = tableMap["username"]!!
+        val password = tableMap["password"]!!
+        loginPage.populateUsername(username)
+        loginPage.populatePassword(password)
         loginPage.clickLoginButton()
     }
 }
--- src/test/resources/cucumber/features/loginPage/login.feature
+++ src/test/resources/cucumber/features/loginPage/login.feature
@@ -1,6 +1,16 @@
 Feature: Login Page - Login
+  Validate login flow
 
-  Scenario: Login
+  Background:
     Given I am on the "Login" page
+
+  Scenario: Log in with valid credentials
     When I log in
-    Then I am on the "Secure" page
+    Then the "Secure" page should display
+
+  Scenario: Log in with invalid credentials
+    When I log in:
+      | username | password |
+      | invalid  | invalid  |
+    Then the following text should display:
+      | Your username is invalid! |
